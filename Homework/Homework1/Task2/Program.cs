e6c83740433cbf573928592fec8f1797d8f0e3a2
alexey_marusin <lexxx351@mail.ru>
2022-08-19 10:22:46 +0500

    Task2 is complited

--- Homework/Homework1/Task2/Program.cs
+++ Homework/Homework1/Task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число a");
+int a = int.Parse (Console.ReadLine ());
+Console.Write("Введите число b");
+int b = int.Parse (Console.ReadLine ());
+
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+Console.WriteLine(b);
