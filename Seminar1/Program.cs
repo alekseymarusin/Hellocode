0efc6922a344184e2c9b5bc6c2bb092a55344a25
alexey_marusin <lexxx351@mail.ru>
2022-08-19 09:31:05 +0500

    add Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите целое число");
+int x = int.Parse (Console.ReadLine ());
+if (x==0)
+{
+Console.WriteLine("Ноль и в Африке ноль");    
+}
+else    x=x*x;
+Console.WriteLine("Квадрат этого числа будет"+x*x);
