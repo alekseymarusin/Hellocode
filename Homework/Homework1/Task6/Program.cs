7c88f7476fcd98b15ee9f4a2e284eb76348ea68a
alexey_marusin <lexxx351@mail.ru>
2022-08-19 12:01:28 +0500

    Add Task6

--- Homework/Homework1/Task6/Program.cs
+++ Homework/Homework1/Task6/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число a");
+int a = int.Parse (Console.ReadLine ());
+
+if (a % 2 == 0)
+{
+    Console.Write("Да");
+}
+
+else
+
+Console.Write("Нет");
