d05293f63198c84dc0a2044cdf8e5ed8406d2b0c
alexey_marusin <lexxx351@mail.ru>
2022-08-22 13:11:18 +0500

    Seminar2+

---
+++ Seminars/Seminar2/Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintDigit()
+{
+int x = new Random().Next(10,100);
+Console.WriteLine($"Случайное число сгенерировалось {x}");
+int x1 = x/10;
+int x2 = x%10;
+if (x1>x2) Console.WriteLine(x1);
+else Console.WriteLine(x2);
+}
+PrintDigit();
