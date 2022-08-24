01fdb3599ca20c3f38ba12c3c5c4cc2a87c9cbe7
alexey_marusin <lexxx351@mail.ru>
2022-08-24 11:50:20 +0500

    Seminar2++

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -1,10 +1,22 @@
 ﻿void PrintDigit()
 {
-int x = new Random().Next(10,100);
-Console.WriteLine($"Случайное число сгенерировалось {x}");
-int x1 = x/10;
-int x2 = x%10;
-if (x1>x2) Console.WriteLine(x1);
-else Console.WriteLine(x2);
+Console.WriteLine("Введите первое число");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int x2 = Convert.ToInt32(Console.ReadLine());
+if (x1 > x2) 
+{
+   if (x1 % x2 == 0) 
+   Console.WriteLine("Кратно");
+   else
+   Console.WriteLine("Некратно, остаток " + x1%x2);
+}
+else 
+{
+    if (x2 % x1 == 0)
+    Console.WriteLine("Кратно");
+    else 
+    Console.WriteLine("Некратно, остаток " + x2%x1);
+}
 }
 PrintDigit();
