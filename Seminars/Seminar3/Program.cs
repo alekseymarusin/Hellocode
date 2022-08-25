4887cb4807962815bf84da47ec01649a02f1eec7
alexey_marusin <lexxx351@mail.ru>
2022-08-25 12:55:26 +0500

    seminar3

---
+++ Seminars/Seminar3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+int GetNum4()
+        {
+            Console.Write("Введите четверть ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату y ");
+            int y = int.Parse(Console.ReadLine());  
+            int res;
+            if ( x>0 && y>0 )   res=1;
+            else if ( x<0 && y>0 )   res=2;
+            else if ( x<0 && y<0 )   res=3;
+            else if ( x>0 && y<0 )   res=4; 
+            else res=0; 
+            return res;
+        }
+        
+        try
+            {
+            Console.WriteLine(GetNum4());
+            }
+        catch
+            {
+            Console.WriteLine("Надо было вводить именно числа, и именно целые!");    
+            }
+
+
+
+
---
+++ Seminars/Seminar3/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите четверть ");
+int Q = int.Parse(Console.ReadLine());
+if (Q == 1) Console.WriteLine("x > 0 и y > 0");
+else if ( Q == 2 ) Console.WriteLine("x < 0 и y > 0");
+else if ( Q == 3 ) Console.WriteLine("x < 0 и y < 0");
+else if ( Q == 4 ) Console.WriteLine("x > 0 и y < 0"); 
+else Console.WriteLine("Область должна быть от 1 до 4");
+
+try
+
+catch
+{
+Console.WriteLine("Надо было вводить числа от 1 до 4, и именно целые!");    
+}
+
