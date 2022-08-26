e9b9516f3678d8fef4ac40a96868a1c3551ab3c9
alexey_marusin <lexxx351@mail.ru>
2022-08-26 10:34:45 +0500

    seminar3

--- Seminars/Seminar3/Task18/Program.cs
+++ Seminars/Seminar3/Task18/Program.cs
@@ -1,17 +1,24 @@
-﻿try
+﻿//Задача 18: Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y)
+
+string NumQ()
     {
     Console.Write("Введите четверть ");
     int Q = Convert.ToInt32(Console.ReadLine());
-    if (Q == 1) Console.WriteLine("x > 0 и y > 0");
-    else if ( Q == 2 ) Console.WriteLine("x < 0 и y > 0");
-    else if ( Q == 3 ) Console.WriteLine("x < 0 и y < 0");
-    else if ( Q == 4 ) Console.WriteLine("x > 0 и y < 0"); 
-    else Console.WriteLine("Область должна быть от 1 до 4");
+    string res = "0";
+    if (Q == 1) res = "x > 0 и y > 0";
+    else if ( Q == 2 ) res = "x < 0 и y > 0";
+    else if ( Q == 3 ) res = "x < 0 и y < 0";
+    else if ( Q == 4 ) res = "x > 0 и y < 0"; 
+    else res = "Quater nr must be within 1-4";
+    return res;
+    };
+try
+    {
+    Console.WriteLine(NumQ());
     }
-Console.WriteLine(" ");
-
 catch
-{
+    {
     Console.WriteLine("Надо было вводить числа от 1 до 4, и именно целые!");    
-}
+    }
 
