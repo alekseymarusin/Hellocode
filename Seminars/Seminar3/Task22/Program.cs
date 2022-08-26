d3bebf1791a1b2586737b98065f1c1ab211857f8
alexey_marusin <lexxx351@mail.ru>
2022-08-26 12:49:33 +0500

    Task22

--- Seminars/Seminar3/Task22/Program.cs
+++ Seminars/Seminar3/Task22/Program.cs
@@ -1,4 +1,19 @@
 ﻿//Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 
-int N = Convert.ToInt32(Console.WriteLine());
-while (N<0)
+void SquareTableN()
+{
+    Console.Write("Введите чиcло ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    for (int i = 1; i < (N+1); i++)
+    {
+        Console.Write($"{i * i}, ");
+    }
+}
+try
+{
+    SquareTableN();
+}
+catch
+{
+    Console.WriteLine("Надо было вводить целые числа");
+}
