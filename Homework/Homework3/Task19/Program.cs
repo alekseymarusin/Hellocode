12abed6ff3e03f6c02ef4a2653c7a226ef7aa954
alexey_marusin <lexxx351@mail.ru>
2022-08-31 09:46:18 +0500

    Task19

--- Homework/Homework3/Task19/Program.cs
+++ Homework/Homework3/Task19/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Palindrom()
+{
+    Console.Write("Введите пятизначное число ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if ((x < 99999) && (x > 10000))
+        {
+        int x1 = x / 10000;
+        int x2 = (x / 1000) % 10; 
+        int x4 = (x / 10) % 10;
+        int x5 = x % 10;
+        if ((x1 == x5) && (x2 == x4))
+            Console.WriteLine("Это число является палиндромом");
+        else
+            Console.WriteLine("Это число не является палиндромом");
+        }
+    else
+        Console.WriteLine("Нужно ввести именно пятизначное число!");
+}
+try
+{
+    Palindrom();
+}
+catch
+{
+    Console.WriteLine("Нужно было вводить пятизначное число!");
+}
