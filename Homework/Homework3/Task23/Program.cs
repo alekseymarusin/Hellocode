6871b04670f217b01e61a4739e8930392064f127
alexey_marusin <lexxx351@mail.ru>
2022-08-31 09:59:04 +0500

    Task23

--- Homework/Homework3/Task23/Program.cs
+++ Homework/Homework3/Task23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void TableOfCubesN()
+{
+    Console.Write("Введите чиcло ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    for (int i = 1; i < (N+1); i++)
+    {
+        Console.Write($"{i * i * i}, ");
+    }
+}
+try
+{
+    TableOfCubesN();
+}
+catch
+{
+    Console.WriteLine("Надо было вводить целые числа");
+}
--- Seminars/Seminar3/Task22/Program.cs
+++ Seminars/Seminar3/Task22/Program.cs
@@ -1,17 +1,17 @@
 ﻿//Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 
-void SquareTableN()
+void TableOfCubesN()
 {
     Console.Write("Введите чиcло ");
     int N = Convert.ToInt32(Console.ReadLine());
     for (int i = 1; i < (N+1); i++)
     {
-        Console.Write($"{i * i}, ");
+        Console.Write($"{i * i * i}, ");
     }
 }
 try
 {
-    SquareTableN();
+    TableOfCubesN();
 }
 catch
 {
