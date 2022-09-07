84c0d8131eed0595d73a6d7a04edf072d1132782
alexey_marusin <lexxx351@mail.ru>
2022-09-07 14:08:00 +0500

    Task29

---
+++ Homework/Homework4/Task29/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int [] numbers = new int[8];
+for(int i = 0; i < 8; i++)
+{
+numbers[i] = new Random().Next(1,100);
+Console.Write($"{numbers[i]}, ");
+}
