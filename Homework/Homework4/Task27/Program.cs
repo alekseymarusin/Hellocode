23f85f860df4ce50eb19bb0171205c9ba8d9ccf8
alexey_marusin <lexxx351@mail.ru>
2022-09-07 14:04:19 +0500

    Task27

---
+++ Homework/Homework4/Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите любое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+int Summa(int x)
+{    
+    int count = Convert.ToString(x).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        advance = x - x % 10;
+        result = result + (x - advance);
+        x = x / 10;
+    }
+   return result;
+}
+try
+{
+    Console.WriteLine("Сумма цифр этого числа " + Summa(x));
+}
+catch
+{
+    Console.WriteLine("Нужно было вводить числа!");
+}
