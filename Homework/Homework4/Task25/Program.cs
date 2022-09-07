59d1d18188dc6f55ff9dc0dfe04751fc533ad071
alexey_marusin <lexxx351@mail.ru>
2022-09-07 13:47:54 +0500

    Task25

--- Homework/Homework4/Task25/Program.cs
+++ Homework/Homework4/Task25/Program.cs
@@ -1,19 +1,22 @@
-﻿int Stepen()
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Stepen()
 {
     Console.WriteLine("Введите число А, которое будут возводить в степень В");
     int A = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Введите число В");
     int B = Convert.ToInt32(Console.ReadLine());
+    int result = A;
     if (B > 0)
     {
-        for (int i = 0; i < B; i++);
+        for(int i = 1; i < B; i++)
         {
-            A = A*A;
+            result = result * A;
         }
     }
     else 
         Console.WriteLine("Число B должно быть натуральным, т.е. 1 или больше");
-    return A;
+    return result;
 }
 try
 {
