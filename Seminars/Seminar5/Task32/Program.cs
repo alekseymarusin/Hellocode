271be7b27893c7da4c5d3c4b0b2302661ad62e5a
alexey_marusin <lexxx351@mail.ru>
2022-09-15 19:14:08 +0500

    Seminar5

--- Seminars/Seminar5/Task32/Program.cs
+++ Seminars/Seminar5/Task32/Program.cs
@@ -3,3 +3,20 @@
 //[-4, -8, 8, 2] -> [4, 8, -8, -2]
 
 
+Console.WriteLine("Задайте число элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+
+for (int i = 0; i < n; i++)
+{
+    array [i] = new Random().Next (-100, 101);
+    Console.Write($"{array[i]}, ");
+}
+Console.WriteLine(" - это изначальный массив");
+for (int i = 0; i < n; i++ )
+{
+    array [i] = array [i] * -1;
+    Console.Write($"{array[i]}, ");
+};
+Console.WriteLine(" - это массив с инвертированными элементами");
+
