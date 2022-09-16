a5a5b88b5d5650f2bfee76942e11f4e51ee2755f
alexey_marusin <lexxx351@mail.ru>
2022-09-16 17:02:03 +0500

    Seminar5

--- Seminars/Seminar5/Task33/Program.cs
+++ Seminars/Seminar5/Task33/Program.cs
@@ -3,3 +3,24 @@
 //-3; массив [6, 7, 19, 345, 3] -> да
 
 
+int[]array = new int[12];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    if (i!=array.Length-1) Console.Write($"{array[i]}, ");
+    else Console.WriteLine($"{array[i]}");
+}
+
+int x = Convert.ToInt32(Console.ReadLine());
+bool find = false;
+foreach (var item in array)
+{
+    if (x == item)
+    {
+        find = true;
+        break;
+    }
+}
+
+if (find) Console.WriteLine("Да");
+else Console.WriteLine ("Нет");
