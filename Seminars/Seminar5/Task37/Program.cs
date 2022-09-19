c2e2eb6c663efd2da0995eddeb0afd2de90b80f6
alexey_marusin <lexxx351@mail.ru>
2022-09-19 19:30:03 +0500

    Seminar5

--- Seminars/Seminar5/Task37/Program.cs
+++ Seminars/Seminar5/Task37/Program.cs
@@ -5,3 +5,28 @@
 // [6 7 3 6] -> 36 21
 
 
+Console.Write("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+for (int i = 0; i < N; ++i)
+    array [i] = new Random().Next(1,10);
+
+Console.Write("Наш сгенерированный массив: [");
+
+for (int i = 0; i < N - 1; ++i)
+    Console.Write(array[i] + " ");
+
+Console.WriteLine(array[N - 1] + "]");
+
+int[] result = new int[N / 2 + N % 2];
+
+for (int i = 0; i < array.Length / 2; ++i)
+    result[i] = array[i] * array[array.Length - 1 - i];
+
+if (array.Length % 2 == 1)
+    result[array.Length / 2] = array[array.Length / 2];
+
+for(int i = 0; i < result.Length - 1; ++i)
+    Console.Write(result[i] + " ");
+
+Console.WriteLine(result[result.Length - 1]);
