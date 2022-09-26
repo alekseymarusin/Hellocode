b0fcc48d12b76fdbac19a7874438a0b43953c1ff
alexey_marusin <lexxx351@mail.ru>
2022-09-26 11:24:06 +0500

    seminar6

---
+++ Seminars/Seminar6/Task39/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int n = 10;
+
+void PrintArray( int[] arr)
+    {
+    for( int i = 0; i < n; i++)
+        {    
+        if (i!=arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.WriteLine($"{arr[i]}");
+        }
+    }
+int [] numbers = new int[n];
+for(int i = 0; i < n; i++)
+    {
+        numbers[i] = new Random().Next(0,100);
+    }
+PrintArray(numbers);
+Console.WriteLine();
+int buf;
+for(int i = 0; i < n/2; i++)
+    {
+        buf = numbers[i];
+        numbers[i] = numbers[n - 1 - i];
+        numbers[n-1-i] = buf;
+    }
+PrintArray(numbers);
