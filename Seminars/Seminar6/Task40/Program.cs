c8da4c0a8df14d85e084194ec4aa857b1e514815
alexey_marusin <lexxx351@mail.ru>
2022-10-04 10:37:12 +0500

    seminar6

---
+++ Seminars/Seminar6/Task40/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+bool IsTrianglePossible(int a, int b, int c)
+{
+    return(a < b + c && b < a + c && c < a + b);
+}
+
+void Task40()
+{
+    Console.Write("Введите первую сторону треугольника: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите вторую сторону треугольника: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите третью сторону треугольника: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"{a}, {b}, {c} - треугольник ");
+    if (!IsTrianglePossible(a,b,c))
+    {
+        Console.Write("не ");
+    }
+    Console.WriteLine("возможен.");
+}
+
+Task40();
+
+
+
+
+
+
---
+++ Seminars/Seminar6/Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int ConvertToBinary(int number)
+{
+    int result = 0;
+    int order = 1;
+    while (number > 0)
+    {
+        result += (number %2) * order;
+        order *= 10;
+        number /= 2;
+    }
+    return result;
+}
+
+void Task41()
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{x} - {ConvertToBinary(x)}");
+}
+
+Task41();
