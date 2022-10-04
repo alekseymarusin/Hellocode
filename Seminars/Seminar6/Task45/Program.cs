391a4f536236eebeacfbfb3fc5a5ce2c72b46653
alexey_marusin <lexxx351@mail.ru>
2022-10-04 11:49:44 +0500

    seminar6

---
+++ Seminars/Seminar6/Task45/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования. */
+
+Console.WriteLine("Insert number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int []arr = new int[N];
+arr[0] = 0;
+arr[1] = 1;
+
+for (int i = 2; i < N; i++)
+{
+arr[i] = arr[i-1] + arr[i-2];
+}
+Console.WriteLine(string.Join(",", arr));
