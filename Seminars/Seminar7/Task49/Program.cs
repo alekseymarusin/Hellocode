362df9b99aa719e0724971910acea23723304803
alexey_marusin <lexxx351@mail.ru>
2022-10-07 10:09:07 +0500

    seminar7

--- Seminars/Seminar7/Task49/Program.cs
+++ Seminars/Seminar7/Task49/Program.cs
@@ -11,3 +11,32 @@
 // 8 4 2 4
 
 
+Console.WriteLine("Введите количество строк двумерного массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] NewMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++) matrix[i, j] = new Random().Next(1, 10);
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] mas = NewMatrix(m, n);
+PrintArray(mas);
