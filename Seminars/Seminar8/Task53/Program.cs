393aa6f20174e75c92edafcfd343d9c9742f363f
alexey_marusin <lexxx351@mail.ru>
2022-10-10 14:50:16 +0500

    seminar8

--- Seminars/Seminar8/Task53/Program.cs
+++ Seminars/Seminar8/Task53/Program.cs
@@ -1,30 +1,45 @@
 ﻿// Задача 53: Задайте двумерный массив. 
 // Напишите программу, которая поменяет местами первую и последнюю строку массива.
 
-int[,] FillArray(int m, int n)
+void FillArray(int[,] table)
 {
-    int[,] array = new int[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++) array[i, j] = new Random().Next(1, 100);
-    }
-    return array;
+Random random = new Random();
+for (int i = 0; i < table.GetLength(0); i++)
+{
+for (int j = 0; j < table.GetLength(1); j++)
+{
+table[i, j] = random.Next(10);
+}
+}
 }
 
-void PrintArray(int[,] array)
+void PrintArray(int[,] table)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
+for (int i = 0; i < table.GetLength(0); i++)
+{
+for (int j = 0; j < table.GetLength(1); j++)
+{
+Console.Write(table[i, j] + "\t");
+}
+Console.WriteLine();
+}
 }
 
-Console.WriteLine("Введите количество строк двумерного массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов двумерного массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите номер строки элемента: ");
+int rows =6,
+cols = 5;
+int [,] table = new int[rows,cols];
+FillArray(table);
+PrintArray(table);
+
+int temp = 0;
+
+for (int i = 0; i < cols; i++)
+{
+temp = table [0,i];
+table [0,i] = table [rows-1,i];
+table [rows-1,i] = temp;
+}
+
+Console.WriteLine();
+
+PrintArray(table);
