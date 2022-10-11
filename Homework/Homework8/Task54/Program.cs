8f2ced8044e1310c8d8c50f2c5e7b30999f15df6
alexey_marusin <lexxx351@mail.ru>
2022-10-11 13:24:10 +0500

    homework8

--- Homework/Homework8/Task54/Program.cs
+++ Homework/Homework8/Task54/Program.cs
@@ -1,57 +1,111 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
-// которая упорядочит по убыванию элементы каждой строки двумерного массива.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+﻿// // Задача 54: Задайте двумерный массив. Напишите программу, 
+// // которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// // Например, задан массив:
+// // 1 4 7 2
+// // 5 9 2 3
+// // 8 4 2 4
+// // В итоге получается вот такой массив:
+// // 7 4 2 1
+// // 9 5 3 2
+// // 8 4 4 2
 
-Console.WriteLine("Введите количество строк");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+// NewMatrix();
+// Matrix1();
 
-int[,] NewMatrix()
+// int[,] NewMatrix()
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             matrix[i, j] = new Random().Next(1, 10);
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+//     return matrix;
+// }
+
+// int[,] Matrix1(int[,] matrix)
+// {
+//     int[,] table = new int[m,n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             int temp;
+//             if(matrix[i,j] > matrix[i,j + 1]) 
+//             {
+//                 temp = matrix[i,j];
+//                 matrix[i,j] = table[i,j+1];
+//                 table[i,j+1] = temp;
+//             }
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+//     return table;
+
+// }
+
+
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив до изменения");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
 {
-    int[,] matrix = new int[m, n];
-    for (int i = 0; i < m; i++)
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
     {
-        for (int j = 0; j < n; j++)
+        for (int z = 0; z < numbers.GetLength(1) - 1; z++)
         {
-            matrix[i, j] = new Random().Next(1, 10);
-            Console.Write($"{matrix[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-    return (matrix);
-}
-
-NewMatrix();
-
-int[,] Matrix1(int[,] matrix)
-{
-    int[,] table = new int[m,n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            int temp;
-            if(matrix[i,j] > matrix[i,j + 1]) 
+            if (numbers[i, z] < numbers[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
             {
-                temp = matrix[i,j];
-                matrix[i,j] = matrix[i,j+1];
-                matrix[i,j+1] = temp;
+                int temp = 0;
+                temp = numbers[i, z];
+                numbers[i, z] = numbers[i, z + 1];
+                numbers[i, z + 1] = temp;
             }
         }
-        Console.WriteLine();
     }
-    Console.WriteLine();
-    return (table);
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+PrintArray(numbers);
 
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
 }
 
-Matrix1(table);
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
