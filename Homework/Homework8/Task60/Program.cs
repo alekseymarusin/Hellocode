68ceda1c5de70e393de84720befc1af35b011cf6
alexey_marusin <lexxx351@mail.ru>
2022-10-13 09:20:13 +0500

    homework8

---
+++ Homework/Homework8/Task60/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Write("Введите размерность массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+NewMatrix();
+
+int[,,] NewMatrix()
+{
+    int[,,] matrix = new int[m, m, m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < m; k++)
+            {
+            matrix[i, j, k] = new Random().Next(10, 100);
+            Console.Write($"{matrix[i, j, k]} {(i,j,k)} ");
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return (matrix);
+}
